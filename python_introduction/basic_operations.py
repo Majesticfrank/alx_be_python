3c5e3796d2fe4c94fa648c9044c4bfb5ca1d72db
fran utibe joseph <majesticfrank7@gmail.com>
2025-07-13 01:05:52 +0100

    changes format

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,9 @@
-number1 = 10 , number2 = 5
-
+number1 = 10 
+number2 = 5
 Addition = number1+number2 
 Subtraction = number2 - number1
 Multiplication = number1 *number2 
+print(f"number1={number1}, number2={number2}")
 print(f"Addition of {number1} and {number2} is {Addition}")
 print(f"Subtraction of {number1} and {number2} is {Subtraction}")
 print(f"Multiplication of {number1} and {number2} is {Multiplication}")
