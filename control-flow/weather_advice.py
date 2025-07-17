c0c3b25df0a6b55ba5b2d6edc3e93b738f341cc1
fran utibe joseph <majesticfrank7@gmail.com>
2025-07-17 02:57:43 +0100

    added weather_advice.py

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,13 @@
+weather_condition = input("what's the weather like toay?(sunny/rainy/cold): ")
+sunny ="Wear a t-shirt and sunglasses."
+rainy ="Don't forget your umbrella and a raincoat."
+cold ="Make sure to wear a warm coat and a scarf."
+
+if weather_condition == "sunny":
+    print(f"recommendation:{sunny}")
+elif weather_condition =="rainy":
+    print(f"recommendation:{rainy}")
+elif weather_condition =="cold":
+    print(f"recommendation:{cold}")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
