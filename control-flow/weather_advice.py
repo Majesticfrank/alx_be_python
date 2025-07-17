3602b4a834e709d88598c575289c82ae12f8efbd
fran utibe joseph <majesticfrank7@gmail.com>
2025-07-17 03:14:53 +0100

    added weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,13 +1,10 @@
 weather_condition = input("What's the weather like today? (sunny/rainy/cold): ")
-sunny ="Wear a t-shirt and sunglasses."
-rainy ="Don't forget your umbrella and a raincoat."
-cold ="Make sure to wear a warm coat and a scarf."
 
 if weather_condition == "sunny":
-    print(f"recommendation:{sunny}")
+    print("Wear a t-shirt and sunglasses.")
 elif weather_condition =="rainy":
-    print(f"recommendation:{rainy}")
+    print("Don't forget your umbrella and a raincoat.")
 elif weather_condition =="cold":
-    print(f"recommendation:{cold}")
+    print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
