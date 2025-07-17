740fba81e0d5f8950982accb6d16845e4626098b
fran utibe joseph <majesticfrank7@gmail.com>
2025-07-17 03:10:33 +0100

    added weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,4 +1,4 @@
-weather_condition =input(" What's the weather like today? (sunny/rainy/cold): ")
+weather_condition = input("What's the weather like today? (sunny/rainy/cold): ")
 sunny ="Wear a t-shirt and sunglasses."
 rainy ="Don't forget your umbrella and a raincoat."
 cold ="Make sure to wear a warm coat and a scarf."
