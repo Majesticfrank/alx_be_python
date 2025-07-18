933ede4ed3dddd44adacaee56c2243494d6fbfef
fran utibe joseph <majesticfrank7@gmail.com>
2025-07-18 01:38:04 +0100

    added match_case_calculator.py

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,18 @@
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+operation =input("choose the operation( +, -, *,/)" )
+
+match operation:
+    case "+":
+        print("The result is", num1 + num2)
+    case "-":
+        print("The result is ", num1 - num2)
+        
+    case "*":
+        print("The result is ", num1 * num2)
+    case "/":
+        if num2 !=0:
+            print("The result is ", num1 / num2)
+        else:
+            print("Error! cannot be divisible")
+        
