16b0e169b85c45727d9f569078c9b5818081704d
fran utibe joseph <majesticfrank7@gmail.com>
2025-07-26 02:37:49 +0100

    added arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,9 +6,9 @@ def perform_operation(num1, num2, operation):
        elif operation == "multiply":
               return num1 * num2
        elif operation == "divide":
-              if num2 != 0:
-                     return num1 / num2
-              else: return "Error cannot divide by zero"
+              if num2 == 0:
+                     return "Error cannot divide by zero" 
+              else: return num1 / num2
        else:
               return "Invalid operation"
        
