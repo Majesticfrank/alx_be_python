9b3d0d8b88f96454595d3145f6b1bdc7b9b0b379
fran utibe joseph <majesticfrank7@gmail.com>
2025-07-27 17:22:26 +0100

    added  explore_datetime.py

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -2,17 +2,18 @@ from datetime import datetime, timedelta
 
 def display_current_datetime():
     current_date = datetime.now() 
-    formatted_date = current_date.strftime("%A, %d %B %Y, %I:%M %p") 
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S") 
     print(f"Current date and time: {current_date}")
     print(f"Formatted date and time: {formatted_date}")
     return current_date 
 def calculate_future_date():
+        from datetime import datetime, timedelta
 
         days = int(input("Enter the number of days to add to the current date (1-31): "))
         if 0 < days <= 31:
             current_date = datetime.now()
-            future_date = current_date + datetime.timedelta(days=days)
-            print(f"Future date after {days} days: {future_date.strftime('%A, %d %B %Y, %I:%M %p')}")
+            future_date = current_date + timedelta(days=days)
+            print(f"Future date after {days} days: {future_date.strftime("%Y-%m-%d %H:%M:%S")}")
         else:
             print("Invalid input. Please enter a number between 1 and 31.")
     
