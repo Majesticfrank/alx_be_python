438485dd4afec10d15655249d24756e47ea518cd
fran utibe joseph <majesticfrank7@gmail.com>
2025-08-02 05:11:54 +0100

    bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,7 +15,7 @@ class BankAccount:
              return True
    
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:2f}")
 
 if __name__ == "__main__":
     my_account =BankAccount(0)
