732aa6b4b0ffe99b0daf73ca0ce7cd7a1b2f660e
fran utibe joseph <majesticfrank7@gmail.com>
2025-08-03 10:01:29 +0100

    added test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,7 +4,7 @@ from simple_calculator import SimpleCalculator
 class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2,3),5)
         self.assertEqual(self.calc.add(-2,3),1)
         self.assertEqual(self.calc.add(-2,-3),-5)
