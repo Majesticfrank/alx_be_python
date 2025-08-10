938a287e6c9c070fe899084310fd71caab06f7ca
fran utibe joseph <majesticfrank7@gmail.com>
2025-08-10 13:09:57 +0100

    added polymorphism_demo.py

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -13,10 +13,9 @@ class Rectangle(Shape):
 
 class Circle(Shape):
    def __init__(self, radius):
-      self.radius =radius
-
-def area(self):
-   return math.pi * self.radius ** 2
+      self.radius = radius
+   def area(self):
+    return math.pi * self.radius ** 2
 def main():
     shapes = [
         Rectangle(10, 5),
