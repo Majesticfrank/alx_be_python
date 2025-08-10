30472898018d5d3a0013e19d2a7eb413a6393270
fran utibe joseph <majesticfrank7@gmail.com>
2025-08-10 06:31:32 +0100

    added library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -12,7 +12,7 @@ class EBook(Book):
       self.file_size = int(file_size)
 
     def __str__(self):
-        return f"EBook: {self.title} by {self.author}, File size: {self.file_size}KB"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
 
@@ -22,7 +22,7 @@ class PrintBook(Book):
         self.page_count=int(page_count)
 
     def __str__(self):
-        return f"printBook: {self.title} by {self.author}, Page count: {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
     def __init__(self):
