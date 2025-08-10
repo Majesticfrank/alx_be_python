2828c06fdc219f29c449ff503007bd5905ed139e
fran utibe joseph <majesticfrank7@gmail.com>
2025-08-10 06:14:40 +0100

    added library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,7 +2,7 @@ class Book:
     def __init__(self, title, author ):
         self.title =title
         self .author =author
-        
+
     def __str__(self):
         return f"{self.title} by {self.author}"
 
@@ -19,7 +19,7 @@ class PrintBook(Book):
 
 class Library:
     def __init__(self):
-        self.books =[]
+        self.books = []
 
     def add_book(self, book):
         if isinstance(book, Book):
